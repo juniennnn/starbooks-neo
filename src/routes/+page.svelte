6d9f5900eff6d219ea60892bdadc80554876a392
hs090925 <hs090925kim@gmail.com>
2026-06-02 16:36:07 +0900

    메뉴 변경

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@ let table = $state('');
 let name = $state('');
 let menu = $state('');
 
-const menus = ['탄산 아이스티', '탄산 레모네이드'];
+const menus = ['탄산 아이스티', '탄산 레모네이드', '아메리카노'];
 let pending = $state(false);
 
 async function submitOrder() {
